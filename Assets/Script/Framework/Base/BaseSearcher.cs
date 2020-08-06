0f24aaeb06b5771aa0fff2bf6c0e3b1fe68dd742
ikegawa <kd1229058@st.kobedenshi.ac.jp>
2020-08-07 03:30:36 +0900

    【Add】 ・WASDのキー操作での移動処理を追加 ・キャラにメインカメラが追従するように処理を追加（まだ上下にバグあり）

---
+++ Assets/Script/Framework/Base/BaseSearcher.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseSearcher : MonoBehaviour
+{
+    private List<GameObject> m_targets = new List<GameObject>();
+
+    protected bool isFound;
+
+    protected GameObject target = null;
+
+    // Start is called before the first frame update
+
+    protected void Init()
+    {
+        var searching = GetComponentInChildren<SightComponent>();
+        searching.onFound += OnFound;
+        searching.onLost += OnLost;
+    }
+
+    private void OnFound(GameObject _foundObject)
+    {
+        m_targets.Add(_foundObject);
+        if (_foundObject.name == "unitychan")
+        {
+            isFound = true;
+            target = _foundObject;
+        }
+    }
+
+    private void OnLost(GameObject _lostObject)
+    {
+        m_targets.Remove(_lostObject);
+
+        if (m_targets.Count == 0)
+        {
+            isFound = false;
+        }
+    }
+}
--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -2,17 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : BaseCompornent
 {
+    [SerializeField]
+    float movePower = 1.0f;
+
+    [SerializeField]
+    float cameraMove = 0.1f;
+
+    Camera playerCamera = null;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        var obj = GameObject.Find("Main Camera");
+        playerCamera = obj.GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            var rot = transform.rotation;
+            rot.x = playerCamera.transform.rotation.x;
+
+            var move = transform.forward * movePower;
+            transform.Translate(move);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            var move = transform.forward * movePower * -1;
+            transform.Translate(move);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            var move = transform.right * movePower * - 1;
+            transform.Translate(move);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            var move = transform.right * movePower;
+            transform.Translate(move);
+        }
+
+        //カメラ操作
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            var axis = transform.up * - 1;
+            playerCamera.transform.RotateAround(Position, axis, cameraMove);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            var axis = transform.up;
+            playerCamera.transform.RotateAround(Position, axis, cameraMove);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var axis = transform.right * -1;
+            playerCamera.transform.RotateAround(Position, axis, cameraMove);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            var axis = transform.right;
+            playerCamera.transform.RotateAround(Position, axis, cameraMove);
+        }
     }
 }
