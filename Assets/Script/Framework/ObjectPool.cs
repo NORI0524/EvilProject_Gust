06fa7853df5666f269ce97c05dc40c9fa4bcee3e
ikegawa <kd1229058@st.kobedenshi.ac.jp>
2020-08-07 09:15:06 +0900

    【Add】 ・オブジェクトプールのクラスを追加 ・スポナークラスの追加

---
+++ Assets/Script/Framework/ObjectPool.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [SerializeField]
+    int InitObjectMax = 10;
+
+    [SerializeField]
+    GameObject targetObject = null;
+
+    List<GameObject> objectList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectList = new List<GameObject>();
+        objectList.Clear();
+
+        for(int index = 0; index < InitObjectMax; index++)
+        {
+            var obj = Instantiate(targetObject) as GameObject;
+            obj.SetActive(false);
+            objectList.Add(obj);
+        }
+    }
+
+    public GameObject GenerateInstance()
+    {
+        var newObject = CheckInactiveObject();
+        if(newObject == null)
+        {
+            newObject = Instantiate(targetObject) as GameObject;
+            newObject.SetActive(true);
+            objectList.Add(newObject);
+        }
+        else
+        {
+            newObject.SetActive(true);
+        }
+        return newObject;
+    }
+
+    GameObject CheckInactiveObject()
+    {
+        GameObject gameObject = null;
+        foreach(var obj in objectList)
+        {
+            if (obj.activeSelf) continue;
+            gameObject = obj;
+            break;
+        }
+        return gameObject;
+    }
+}
