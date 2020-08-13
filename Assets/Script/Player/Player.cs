1b985c53d1fb53857180b397418679a5cf0df7a2
ikegawa <kd1229058@st.kobedenshi.ac.jp>
2020-08-13 11:19:26 +0900

    【Add】 ・Randomクラスの拡張メソッドを追加 ・Timerクラスの秒数を小数で指定できるよう修正

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -17,6 +17,8 @@ public class Player : BaseCompornent
     Rigidbody playerRigidbody = null;
     Animator playerAnimator = null;
 
+    Random random;
+
     // Start is called before the first frame update
     void Start()
     {
