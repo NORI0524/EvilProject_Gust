4e7f0388375695f050ba3d169724c2d4f4354eb1
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-08-19 15:43:13 +0900

    【Add】 ・BaseWeaponスクリプトの追加

---
+++ Assets/Script/Wepon/BaseWeapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseWeapon : MonoBehaviour
+{
+    [SerializeField]
+    float damage = 0.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // 当たり判定
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(damage);
+    }
+}
