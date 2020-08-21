1be4719b1851750be7b02cf625ca3802d1e5c5b2
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-08-21 12:07:08 +0900

    【Add,Fix】 ・ダメージ判定をテストするためのスクリプトを追加 ・BaseWpeaponの修正 ∟ダメージ処理の追加

--- Assets/Script/Wepon/BaseWeapon.cs
+++ Assets/Script/Wepon/BaseWeapon.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class BaseWeapon : MonoBehaviour
 {
-    [SerializeField]
-    float damage = 0.0f;
+    [SerializeField] int power = 1;          // 攻撃力
+    [SerializeField] int criticalRate = 0;   // クリティカル率
 
-
-    // Start is called before the first frame update
-    void Start()
+    // ダメージ通知書
+    private struct DamageArg
     {
-        
+        public int atkPower;
+    }
+
+    // ダメージ通知返信処理
+    public struct DamageReply
+    {
+        public bool isGuard;           // 防御成功の可否
     }
 
     // 当たり判定
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(damage);
+        //-------------------
+        // ダメージ処理
+        //-------------------
+        // 通知用データ
+        DamageArg dmg = new DamageArg();
+        dmg.atkPower = power;
+        // 返信用データ
+        DamageReply rep;
+
+        // 相手にダメージ情報を通知する
+        //var you=other.GetComponent<damagetest>().isDamage(dmg,rep);
+        
+
+        // デバッグ
+        Debug.Log(dmg.atkPower);
     }
 }
---
+++ Assets/Script/Wepon/damagetest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagetest : MonoBehaviour
+{
+    [SerializeField] int hp = 100;
+
+    // ダメージ通知書
+    public struct DamageArg
+    {
+        public int atkPower;    // 攻撃の威力
+    }
+
+    // ダメージ通知返信処理
+    public struct DamageReply
+    {
+        public bool isGuard;           // 防御成功の可否
+    }
+
+    // ダメージ通知
+    // dmg...攻撃者から送られるダメージ通知データ
+    // rep...攻撃者へ返信する内容
+    public bool isDamage(DamageArg dmg, DamageReply rep)
+    {
+        // HPを減らす
+        hp -= dmg.atkPower;
+
+        // 防御しない
+        rep.isGuard = false;
+
+        return true;
+    }
+
+}
