a9f574a8997d486fb1a391f01b14786645d1b70e
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-08-25 15:49:03 +0900

    【Fix】 ・ダメージ処理をより汎用的に改造

--- Assets/Script/Wepon/BaseWeapon.cs
+++ Assets/Script/Wepon/BaseWeapon.cs
@@ -2,22 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseWeapon : MonoBehaviour
+public class BaseWeapon : testBase
 {
     [SerializeField] int power = 1;          // 攻撃力
     [SerializeField] int criticalRate = 0;   // クリティカル率
-
-    // ダメージ通知書
-    private struct DamageArg
-    {
-        public int atkPower;
-    }
-
-    // ダメージ通知返信処理
-    public struct DamageReply
-    {
-        public bool isGuard;           // 防御成功の可否
-    }
 
     // 当たり判定
     private void OnTriggerEnter(Collider other)
@@ -29,11 +17,10 @@ public class BaseWeapon : MonoBehaviour
         DamageArg dmg = new DamageArg();
         dmg.atkPower = power;
         // 返信用データ
-        DamageReply rep;
+        DamageReply rep = new DamageReply();
 
         // 相手にダメージ情報を通知する
-        //var you=other.GetComponent<damagetest>().isDamage(dmg,rep);
-        var you=other.GetComponent<damagetest>().isDamage(dmg.atkPower);
+        var you=other.GetComponent<damagetest>().isDamage(dmg,rep);
         
         // デバッグ
         Debug.Log(dmg.atkPower);
