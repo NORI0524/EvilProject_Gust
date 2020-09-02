e4002977e6e19b93f2c2c695727b042c61530b05
tomoka313 <kd1237098@st.kobedenshi.ac.jp>
2020-09-02 09:59:17 +0900

    【Add】・敵の移動処理と索敵処理の追加

---
+++ Assets/Script/Enemy/CapsuleController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleController : MonoBehaviour
+{
+    float moveSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int UDkey = 0;
+        int RLKey = 0;
+        if (Input.GetKey(KeyCode.UpArrow)) { UDkey = 1; }
+        if (Input.GetKey(KeyCode.DownArrow)) { UDkey = -1; }
+        if (Input.GetKey(KeyCode.RightArrow)) { RLKey = 1; }
+        if (Input.GetKey(KeyCode.LeftArrow)) { RLKey = -1; }
+
+        if (UDkey != 0)
+        {
+            transform.position += new Vector3(0, 0, UDkey * moveSpeed);
+        }
+        if (RLKey != 0)
+        {
+            transform.position += new Vector3(RLKey * moveSpeed, 0, 0);
+        }
+    }
+}
---
+++ Assets/Script/Enemy/Navigation.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigation : MonoBehaviour
+{
+    // NavMeshAgent取得
+    NavMeshAgent agent;
+    Enemy e_con;
+
+    // MOVE
+    [SerializeField] public GameObject[] Target;    // 目的地の配列
+    private int targetCount = 0; // 現在の目的地番号
+
+    //CHASE
+    [SerializeField]
+    Transform Player = default;    // プレイヤーの座標
+    [SerializeField]
+    float dist = 5.0f;    // 索敵範囲(範囲外に出たら追尾終了)
+
+    // フラグ
+    bool tracking = false;  // プレイヤーを追尾しているかどうか
+    bool searching = false; // 見失った時にSearchするかどうか
+    bool moving = false;    // ステージ内を移動するかどうか
+
+    private Vector3 saveTargetPos;  // 見失う直前のプレイヤーの座標を保存
+    
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        e_con = GetComponent<Enemy>();
+    }
+    
+    void Update()
+    {
+        if (tracking)
+        {
+            float distance = Vector3.Distance(this.transform.position, Player.transform.position);
+            if (distance >= dist) { e_con.EndDiscover(); }
+        }
+
+        if (moving)
+        {
+            // 目的地にたどり着いたら次の目的地を設定する
+            if (this.transform.position.x == Target[targetCount].transform.position.x && this.transform.position.z == Target[targetCount].transform.position.z)
+            {
+                targetCount++;
+                if (targetCount > Target.Length - 1)
+                {
+                    targetCount -= Target.Length;
+                }
+                agent.SetDestination(Target[targetCount].transform.position);
+            }
+        }
+    }
+
+    // 追尾
+    public void StartTracking()
+    {
+        moving = false;
+
+        agent.speed = 2.0f;
+        agent.angularSpeed = 150;
+        tracking = true;
+        agent.isStopped = false;
+        if (agent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            // ターゲットの座標を設定
+            agent.SetDestination(Player.transform.position);
+        }
+    }
+
+    // ステージ内の移動
+    public void StartMoving()
+    {
+        moving = true;
+
+        agent.speed = 1.0f;
+        agent.angularSpeed = 120;
+        tracking = false;
+        agent.isStopped = false;
+        if (agent.pathStatus != NavMeshPathStatus.PathInvalid)
+        {
+            // 目的地の座標を設定
+            agent.SetDestination(Target[targetCount].transform.position);
+        }
+    }
+}
