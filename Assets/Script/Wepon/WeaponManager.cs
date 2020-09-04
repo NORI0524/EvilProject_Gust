5d337214ce72cf021ebdc217fae276abbf1c0e12
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-09-04 10:52:33 +0900

    【Add,Fix】 ・Resourcesフォルダ追加 ・Resourcesフォルダに武器のPrefabフォルダを移動 ・WeaponManagerスクリプトの追加 ・WeaponManagerの動作確認用スクリプト（testScript）の追加

---
+++ Assets/Script/Wepon/WeaponManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class WeaponManager : MonoBehaviour
+{
+    // 武器変更の関数
+    // 
+    public void WeaponChange()
+    {
+        // 現在の武器を削除
+        // ゲームオブジェクトの取得
+        GameObject currentWeapon = GameObject.Find("dark_sword");
+        Destroy(currentWeapon);
+
+        // プレハブのロード
+        GameObject prf = (GameObject)Resources.Load("Prefabs/dark_sword");
+        if(prf == null)
+        {
+            Debug.LogError("Prefabがロードされませんでした");
+        }
+
+        Vector3 weaponPos = GameObject.Find("dark_sword").transform.position;
+
+        // プレハブをインスタンス化
+        GameObject gameObject = (GameObject)Instantiate(prf, weaponPos, Quaternion.identity);
+    }
+}
