d5569fc8619b9456308206490366ae0c11bb4038
Art20-hub <66098055+Art20-hub@users.noreply.github.com>
2020-09-11 09:51:22 +0900

    【Add】武器の表示 非表示を追加

---
+++ Assets/Script/Input/WeaponDelete.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponDelete : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.X))
+        {
+            foreach (Transform item in this.gameObject.transform)
+            {
+                item.gameObject.SetActive(true);
+            }
+        }
+        if (Input.GetKey(KeyCode.C))
+        {
+            foreach (Transform item in this.gameObject.transform)
+            {
+                item.gameObject.SetActive(false);
+            }
+        }
+    }
+}
