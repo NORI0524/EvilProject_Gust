8b8ca41d7b8e5c3f9603b82a85c35673647c866f
Art20-hub <66098055+Art20-hub@users.noreply.github.com>
2020-09-13 14:36:04 +0900

    【Fix】武器のやつをちょっと修正

---
+++ Assets/Script/Input/WeaponScript/WeaponCircle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCircle : MonoBehaviour
+{
+    // 回転の速さ
+    [SerializeField] float Angle = 2f;
+    // 追従するオブジェクト
+    [SerializeField] GameObject FollowObj = null;
+
+    void Start()
+    {
+        if (FollowObj != null)
+        {
+            transform.position = FollowObj.transform.position;
+            transform.localEulerAngles = Vector3.zero;
+        }
+    }
+
+    void Update()
+    {
+       
+        transform.position = FollowObj.transform.position;
+        
+        transform.Rotate(0, -Angle, 0);
+    }
+}
