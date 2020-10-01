24e1f6b6925fc5874d8f41bfd808df7bc0854ee1
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-10-01 13:12:12 +0900

    【Add】 ・sceneChangeMangerの追加 ・Fadeシェーダーの追加

---
+++ Assets/Script/sceneChangeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChangeManager : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene()
+    {
+        SceneManager.LoadScene("Scenes/TestScene/StageTestScene");
+    }
+}
