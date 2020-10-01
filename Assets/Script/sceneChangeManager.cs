41a5add714c81139324fc71d87d559bf2e4f731b
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-10-01 20:59:42 +0900

    【Fix】 ・シーン偏移にフェードシェーダーを使用するように変更

--- Assets/Script/sceneChangeManager.cs
+++ Assets/Script/sceneChangeManager.cs
@@ -5,17 +5,67 @@ using UnityEngine.SceneManagement;
 
 public class sceneChangeManager : MonoBehaviour
 {
+    // フェード用のマテリアル
+    [SerializeField] Material fademateiral;
+    bool isFade = false;
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            ChangeScene();
+            StartCoroutine(ChangeScene());
         }
     }
 
-    void ChangeScene()
+    public IEnumerator ChangeScene()
     {
-        SceneManager.LoadScene("Scenes/TestScene/StageTestScene");
+        // sceneが切り替わっても破棄されないようにする
+        DontDestroyOnLoad(this.gameObject);
+
+        StartCoroutine(FadeOut());
+        // フェードが終わるまで待機
+        while(!isFade)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        SceneManager.LoadScene("Scenes/TestScene/EnemyTestScene");
+
+        StartCoroutine(FadeIn());
+        while (isFade)
+        {
+            yield return new WaitForEndOfFrame();
+        }
     }
+
+    public IEnumerator FadeOut()
+    {
+        for (float degree = 0.0f; degree < 1.0f;)
+        {
+            degree += Time.deltaTime;
+            Debug.Log(degree);
+
+            // マテリアルに値をセット
+            fademateiral.SetFloat("_Degree", degree);
+
+            yield return null;
+        }
+        isFade = true;
+    }
+
+    public IEnumerator FadeIn()
+    {
+        for (float degree = 1.0f; degree > 0.0f;)
+        {
+            degree -= Time.deltaTime;
+            Debug.Log(degree);
+
+            // マテリアルに値をセット
+            fademateiral.SetFloat("_Degree", degree);
+
+            yield return null;
+        }
+        isFade = true;
+    }
+
 }
