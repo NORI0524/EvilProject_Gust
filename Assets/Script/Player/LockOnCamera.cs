4236a2a18ddaa247dffbe61e431e2087a3fbb025
Hrk-Ogs <kd1230545@st.kobedenshi.ac.jp>
2020-10-23 17:02:07 +0900

    【Add】 ・ロックオン処理を実装 ・PostProcessingでシェーダーを追加

---
+++ Assets/Script/Player/LockOnCamera.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockOnCamera : MonoBehaviour
+{
+    private GameObject player = null;
+    private LockOnTargetDetector lockOnTargetDetector = null;
+    private GameObject lockOnTarget = null;
+
+    [SerializeField] protected LockOnCursor lockonCursor;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        lockOnTargetDetector = player.GetComponentInChildren<LockOnTargetDetector>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (lockOnTarget == null)
+            {
+                // ロックオンターゲットを取得
+                GameObject target = lockOnTargetDetector.GetTargetClosestPlayer();
+
+                if (target != null)
+                {
+                    lockOnTarget = target;
+                    lockonCursor.OnlockonStart(target.transform);
+                }
+            }
+            else
+            {
+                // すでにターゲットが設定されていた場合は解除
+                // 視点リセット
+                transform.LookAt(player.transform.eulerAngles);
+
+                lockOnTarget = null;
+                lockonCursor.OnlockonEnd();
+            }
+        }
+
+        if (lockOnTarget)
+        {
+            lockOnTargetObject(lockOnTarget);
+        }
+    }
+
+    private void lockOnTargetObject(GameObject target)
+    {
+        transform.LookAt(target.transform, Vector3.up);
+    }
+
+}
