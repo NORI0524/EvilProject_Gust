1d28392205fe645560b7f0be2ff522d4add9a866
ikegawa <kd1229058@st.kobedenshi.ac.jp>
2020-11-04 14:15:34 +0900

    【Add】 ・コライダー接触時に指定のエフェクトを出すコンポーネントの作成

---
+++ Assets/Script/Effect/ColliderHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderHit : MonoBehaviour
+{
+    [SerializeField] private GameObject targetObject = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var pos = this.transform.position;
+        var hitPos = other.ClosestPointOnBounds(pos);
+        var hitVec = pos - hitPos;
+        var quaternion = Quaternion.LookRotation(hitVec.normalized);
+        var createObject = GameObject.Instantiate(targetObject);
+        createObject.transform.position = hitPos;
+        createObject.transform.rotation = quaternion;
+    }
+}
